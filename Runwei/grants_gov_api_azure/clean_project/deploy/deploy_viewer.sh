a06e56a9940a0b7d751a2fb1c469e336ed032a9a
Hali Ding <dhaliii0321@gmail.com>
2025-05-12 22:44:32 -0400

    Delete Runwei/grants_gov_api_azure directory

--- Runwei/grants_gov_api_azure/clean_project/deploy/deploy_viewer.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /Users/dinghali/Desktop/Runwei/grants_gov_api_azure/clean_project/src/functions/GrantsGovFunctions
-echo "Deploying updated GrantsViewer function..."
-func azure functionapp publish GrantsGovFunc60542 --force
-echo "Deployment complete. View your grants at:"
-echo "https://grantsgovfunc60542.azurewebsites.net/api/grantsviewer?format=html&limit=1000"
