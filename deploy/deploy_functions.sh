2da1956a7f490f6a3d4f9bdeaa4c7fa2e10a9367
Hali Ding <dhaliii0321@gmail.com>
2025-05-27 13:34:51 -0400

    Delete deploy directory

--- deploy/deploy_functions.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /Users/dinghali/Desktop/Runwei/grants_gov_api_azure/clean_project/src/functions/GrantsGovFunctions
-echo "Deploying functions to Azure..."
-func azure functionapp publish GrantsGovFunc60542 --force
-echo "Deployment complete."
